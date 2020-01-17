a0bdbafd7151ebdb8812a74f8d75bc067783ec49
Aaron <tim2464@yahoo.com.tw>
2020-01-17 11:28:32 -0800

    [Bugfix]

---
+++ Assignment3/app/src/main/java/com/example/assignment3/fragment/ContactsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.assignment3.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.assignment3.R
+
+class ContactsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+
+
+}
